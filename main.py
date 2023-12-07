7abed0a11054d9e77f2a0cd127479a0c0325af82
SakshamKatiy <115263557+SakshamKatiy@users.noreply.github.com>
2023-12-07 15:08:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,40 +2,43 @@ import streamlit as st
 import numpy as np
 import pickle
 
-try:
-    # Loading models
-    dtr = pickle.load(open('dtr.pkl', 'rb'))
-    
-except Exception as e:
-    st.error(f"Error loading models: {e}")
-    dtr = None
-    preprocesser = None
+def load_models():
+    try:
+        # Loading models
+        dtr = pickle.load(open('dtr.pkl', 'rb'))
+        preprocesser = pickle.load(open('preprocesser.pkl', 'rb'))
+        return dtr, preprocesser
+    except Exception as e:
+        st.error(f"Error loading models: {e}")
+        return None, None
+
+def predict_yield(dtr, preprocesser, area, item, year, rainfall, pesticides, temperature):
+    features = np.array([[area, item, year, rainfall, pesticides, temperature]])
+    transformed_features = preprocesser.transform(features) 
+    predicted_value = dtr.predict(transformed_features).reshape(1, -1)
+    return predicted_value
 
 st.title('Crop Yield Prediction Per Country')
 
-st.markdown('## Crop Features Here')
+dtr, preprocesser = load_models()
 
-year = st.number_input('Year')
-average_rain_fall_mm_per_year = st.number_input('Average Rainfall (mm per year)')
-pesticides_tonnes = st.number_input('Pesticides (tonnes)')
-avg_temp = st.number_input('Average Temperature')
+if dtr is not None and preprocesser is not None:
+    st.markdown('## Crop Features Here')
 
+    year = st.number_input('Year')
+    average_rain_fall_mm_per_year = st.number_input('Average Rainfall (mm per year)')
+    pesticides_tonnes = st.number_input('Pesticides (tonnes)')
+    avg_temp = st.number_input('Average Temperature')
 
-area_options = ["Albania", "Algeria", "Angola","Argentina","Bahamas","Bahrain","Bangladesh","Belarus","Canada","Egypt","India","Nepal","New Zealand","Pakistan"]  # Add your actual options here
+    area_options = ["Albania", "Algeria", "Angola","Argentina","Bahamas","Bahrain","Bangladesh","Belarus","Canada","Egypt","India","Nepal","New Zealand","Pakistan"]
 
-area = st.selectbox('Area', area_options)
+    area = st.selectbox('Area', area_options)
 
-item_option = ["Maize","Potatoes","Wheat","Rice, paddy","Soybeans","Sweet potatoes","Cassava","Yams","Plantains and others"] 
-item= st.selectbox('Item',item_option)
+    item_option = ["Maize","Potatoes","Wheat","Rice, paddy","Soybeans","Sweet potatoes","Cassava","Yams","Plantains and others"] 
+    item = st.selectbox('Item', item_option)
 
+    if st.button('Predict'):
+        predicted_value = predict_yield(dtr, preprocesser, area, item, year, average_rain_fall_mm_per_year, pesticides_tonnes, avg_temp)
 
-
-if st.button('Predict'):
-    features = np.array([[area, item, year, average_rain_fall_mm_per_year, pesticides_tonnes, avg_temp]])
-   
-    predicted_value = dtr.predict(transformed_features).reshape(1, -1)
-
-    st.markdown('## Predicted Yield Productions:')
-    st.write(predicted_value)
-
-
+        st.markdown('## Predicted Yield Productions:')
+        st.write(predicted_value)
