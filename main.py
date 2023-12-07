61d10960164bcab2857a3f3167798bc39cf919ba
SakshamKatiy <115263557+SakshamKatiy@users.noreply.github.com>
2023-12-07 15:28:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 import streamlit as st
 import numpy as np
 import pickle
-
-
-from sklearn.linear_model import LinearRegression,Lasso,Ridge
+from sklearn.linear_model import LinearRegression, Lasso, Ridge
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn.tree import DecisionTreeRegressor
-from sklearn.metrics import mean_squared_error,r2_score
-
+from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
+from sklearn.compose import ColumnTransformer
 
 def load_models():
     try:
@@ -19,32 +18,27 @@ def load_models():
         st.error(f"Error loading models: {e}")
         return None, None
 
-def predict_yield(dtr, preprocesser, area, item, year, rainfall, pesticides, temperature):
+def preprocess_data(data):
+    ohe = OneHotEncoder(drop='first')
+    scaler = StandardScaler()
+    preprocesser = ColumnTransformer(
+        transformers=[
+            ('onehotencoder', ohe, [0, 1]),
+            ('standrization', scaler, [2, 3, 4, 5])
+        ],
+        remainder='passthrough'
+    )
+    return preprocesser.fit_transform(data)
+
+def predict_yield(model, preprocesser, area, item, year, rainfall, pesticides, temperature):
     features = np.array([[area, item, year, rainfall, pesticides, temperature]])
-    transformed_features = preprocesser.transform(features) 
-    predicted_value = dtr.predict(transformed_features).reshape(1, -1)
+    transformed_features = preprocesser.transform(features)
+    predicted_value = model.predict(transformed_features).reshape(1, -1)
     return predicted_value
 
 st.title('Crop Yield Prediction Per Country')
 
 dtr, preprocesser = load_models()
-
-models = {
-    'lr':LinearRegression(),
-    'lss':Lasso(),
-    'rg':Ridge(),
-    'Knr':KNeighborsRegressor(),
-    'dtr':DecisionTreeRegressor()
-}
-
-dtr = DecisionTreeRegressor()
-dtr.fit(x_train_dummy,y_train)
-dtr.predict(x_test_dummy)
-for name, mod in models.items():
-    mod.fit(x_train_dummy,y_train)
-    y_pred = mod.predict(x_test_dummy)
-    
-    print(f"{name} MSE : {mean_squared_error(y_test,y_pred)} Score {r2_score(y_test,y_pred)}")
 
 if dtr is not None and preprocesser is not None:
     st.markdown('## Crop Features Here')
@@ -54,27 +48,13 @@ if dtr is not None and preprocesser is not None:
     pesticides_tonnes = st.number_input('Pesticides (tonnes)')
     avg_temp = st.number_input('Average Temperature')
 
-    area_options = ["Albania", "Algeria", "Angola","Argentina","Bahamas","Bahrain","Bangladesh","Belarus","Canada","Egypt","India","Nepal","New Zealand","Pakistan"]
-
+    area_options = ["Albania", "Algeria", "Angola", "Argentina", "Bahamas", "Bahrain", "Bangladesh", "Belarus", "Canada", "Egypt", "India", "Nepal", "New Zealand", "Pakistan"]
     area = st.selectbox('Area', area_options)
 
-    item_option = ["Maize","Potatoes","Wheat","Rice, paddy","Soybeans","Sweet potatoes","Cassava","Yams","Plantains and others"] 
+    item_option = ["Maize", "Potatoes", "Wheat", "Rice, paddy", "Soybeans", "Sweet potatoes", "Cassava", "Yams", "Plantains and others"]
     item = st.selectbox('Item', item_option)
-x_train_dummy = preprocesser.fit_transform(x_train)
-x_test_dummy = preprocesser.transform(x_test)
+
     if st.button('Predict'):
-        from sklearn.preprocessing import OneHotEncoder,StandardScaler
-        from sklearn.compose import ColumnTransformer
-        ohe = OneHotEncoder(drop = 'first')
-        scaler = StandardScaler()
-        preprocesser = ColumnTransformer(
-        transformers = [
-            ('onehotencoder',ohe,[0,1]),
-            ('standrization',scaler,[2,3,4,5])
-        ],
-        remainder = 'passthrough'
-        )
-        
         predicted_value = predict_yield(dtr, preprocesser, area, item, year, average_rain_fall_mm_per_year, pesticides_tonnes, avg_temp)
 
         st.markdown('## Predicted Yield Productions:')
